ea1fa9eb95331b60eb9c329c24d001c059d66b08
zangmakm <zangmakm@gmail.com>
2020-06-27 12:55:01 +1000

    fix add builder function

--- src/controllers/builder.js
+++ src/controllers/builder.js
@@ -45,17 +45,17 @@ async function addBuilder(req, res) {
   });
 
   const user = await userModel.findById(userId).exec();
-  //   if (user.builder) {
-  //     return responseFormatter(
-  //       res,
-  //       400,
-  //       "Builder cannot be registered twice with the same username",
-  //       null
-  //     );
-  //   }
+  if (user.builder) {
+    return responseFormatter(
+      res,
+      400,
+      "Builder cannot be registered twice with the same username",
+      null
+    );
+  }
   builder.user = userId;
   await builder.save();
-  user.role = builder._id;
+  user.builder = builder._id;
   await user.save();
   return formatResponse(res, builder);
 }
